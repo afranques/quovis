3819bdafeb83d8435b603f4d5009828e68298392
Antonio Franques <afranques44@gmail.com>
2016-06-07 11:05:09 -0500

    You can now choose a place from the list and show its details in a new activity. No map yet, just show the coordinates

---
+++ app/src/main/java/com/afranques/quovis/ShowPlaceActivity.java
@@ -0,0 +1,65 @@
+package com.afranques.quovis;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ShowPlaceActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_place);
+
+        //to get the parameters sent from the previous intent
+        Intent prevIntent = getIntent();
+        int category_id = prevIntent.getIntExtra("category_id", -1);
+
+        myDb = new DatabaseHelper(this);
+        Cursor res = myDb.getPlace(category_id);
+        if (res.getCount() != 0) {
+            res.moveToNext();
+
+            TextView placeTitle = (TextView) findViewById(R.id.show_place_title);
+            placeTitle.setText(res.getString(1));
+
+            TextView placeDescription = (TextView) findViewById(R.id.show_place_description);
+            placeDescription.setText(res.getString(2));
+
+            TextView placeCategory = (TextView) findViewById(R.id.show_place_category);
+            placeCategory.setText(res.getString(3));
+
+            TextView placeLatLong = (TextView) findViewById(R.id.show_place_latlong);
+            placeLatLong.setText("Lat: " + res.getString(4) + " Long: " + res.getString(5));
+
+            ImageView img = (ImageView) findViewById(R.id.show_thePicture);
+            String pic_location = res.getString(6);
+            Toast.makeText(this, pic_location, Toast.LENGTH_SHORT).show();
+
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Bitmap bitmap = BitmapFactory.decodeFile(pic_location, options);
+            img.setImageBitmap(bitmap);
+        }
+    }
+
+    public void editPlace(View view) {
+        //
+    }
+
+    public void goToMain(View view) {
+        //
+    }
+}
