7f00eb3e30f4526ab247b63085cdffe2771d7f22
Antonio Franques <afranques44@gmail.com>
2016-06-14 20:20:27 -0500

    Now instead of spinner to choose category view, we have tabs to swipe among them

---
+++ app/src/main/java/com/afranques/quovis/MainContentTabs.java
@@ -0,0 +1,115 @@
+package com.afranques.quovis;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by AMFG on 6/14/2016.
+ */
+public class MainContentTabs extends Fragment {
+
+    DatabaseHelper myDb;
+    List<String> items = new ArrayList<String>();
+    List<Integer> itemsID = new ArrayList<Integer>();
+    private int category_id;
+
+    public MainContentTabs(int category_id) {
+        // Required empty public constructor
+        this.category_id = category_id;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View v = inflater.inflate(R.layout.content_main, container, false);
+        showPlacesCategory(v, category_id);
+        return v;
+    }
+
+    private void showPlacesCategory (View view, int position) {
+        items = new ArrayList<String>();
+        itemsID = new ArrayList<Integer>();
+        myDb = new DatabaseHelper(this.getContext());
+        Cursor res = myDb.getPlacesByCat(position);
+        while (res.moveToNext()) {
+            itemsID.add(res.getInt(0)); //to get the place_id
+            items.add(res.getString(1));
+        }
+
+        //we set the list to its place
+        ArrayAdapter adapter = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_list_item_1, items);
+        ListView listView = (ListView) view.findViewById(R.id.list_places_id);
+        listView.setAdapter(adapter);
+
+        //when we click to an element of the list
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(view.getContext(), ShowPlaceInMapsActivity.class);
+                intent.putExtra("place_id", itemsID.get(position));
+                startActivity(intent);
+                //Toast.makeText(view.getContext(), Integer.toString(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> arg0, final View view, final int pos, long id) {
+                AlertDialog.Builder alert = new AlertDialog.Builder(view.getContext());
+                alert.setTitle("Delete place");
+                alert.setMessage("Place name: "+items.get(pos));
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        //Toast.makeText(view.getContext(), "cat " + itemsID.get(pos)+" deleted", Toast.LENGTH_SHORT).show();
+
+                        //we delete that place_id (entry) from Places
+                        myDb.deletePlace(itemsID.get(pos));
+
+                        //we reload the activity in order to load the new category
+                        Intent intent = getActivity().getIntent();
+                        getActivity().finish();
+                        startActivity(intent);
+                    }
+                });
+                alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        // what ever you want to do with No option.
+                        dialog.cancel();
+                    }
+                });
+                alert.show();
+
+                return true;
+            }
+        });
+    }
+
+}
